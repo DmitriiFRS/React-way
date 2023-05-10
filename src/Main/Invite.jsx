cbe38ac565d0b7a95adfb9d0202a263fe422eaf5
Дмитрий <Dmitriifrs@gmail.com>
2023-05-10 15:40:48 +0500

    Сделал список пользователей для приглашений

--- src/Main/Invite.jsx
+++ src/Main/Invite.jsx
@@ -1,31 +1,64 @@
 import  './App.scss';
 import './Null.css';
 import React from 'react';
-import glass from './loupe.svg';
-import plus from './plus.svg';
+import glass from './icons/loupe.svg';
 import Skeleton from './Utilities/Skeleton';
+import minus from './icons/minus.svg';
+import plus from './icons/plus.svg';
+import InvitePopup from './Utilities/InvitePopup';
 function Invite() {
+
    const [users, setUsers] = React.useState('');
    const [isLoaded, setLoad] = React.useState(false);
+   const [input, setInput] = React.useState('');
+   const [invited, setInvite] = React.useState([]);
+   const [isModalActive, setModalStatus] = React.useState(false);
+   const [invitedCopy, setInviteCopy] = React.useState([]);
+   const sendBtn = React.useRef();
    React.useEffect(() => {
       fetch('https://reqres.in/api/users')
       .then(resp => resp.json())
       .then(data => {
+         console.log(data.data)
          setUsers(data.data);
          setLoad(true)})
       .catch(() => alert('Данные не загружены'))
    }, [])
-   console.log(users)
+   function throwInvite(user) {
+      if (invited.includes(user.id)) {
+         setInvite(invited.filter(item => item !== user.id ))
+      }
+      else 
+      setInvite([...invited, user.id]);
+   }
+   function openPopup() {
+      setModalStatus(!isModalActive);
+      if (!isModalActive) {
+         setInviteCopy(invited);
+         setInvite([]);
+      }
+   }
    return(
       <div className='container'>
          <div className="invite-app">
             <div className="invite-app__body">
                <div className="invite-app__input">
                   <img src={glass} alt="" className="invite-app__loop" />
-                  <input placeholder='Поиск...' type="text" className="invite-app__input-field" />
+                  <input onInput={(e) => setInput(e.target.value)} placeholder='Поиск...' type="text" className="invite-app__input-field" />
                </div>
+               <InvitePopup openPopup={openPopup} modalActive={isModalActive} usersNum={invitedCopy.length}/>
                <div className="invite-app__users">
-                  {isLoaded && users.map((item, index) => {
+                  {!isLoaded ? 
+                  <>
+                  <Skeleton />
+                  <Skeleton />
+                  <Skeleton />
+                  <Skeleton />
+                  </>
+                   : users.filter(item => {
+                     let fullname = item.first_name.toLowerCase() + ' ' + item.last_name.toLowerCase();
+                     return fullname.includes(input.toLowerCase());
+                   }).map((item, index) => {
                      return (
                         <div key={index} className="invite-app__user">
                            <img src={item.avatar} alt="avatar" className="invite-app__img" />
@@ -33,15 +66,14 @@ function Invite() {
                               <h2 className="invite-app__name">{item.first_name} {item.last_name}</h2>
                               <p className="invite-app__email">{item.email}</p>
                            </div>
-                           <img src={plus} alt="add" className="invite-app__add" />
+                           <img onClick={() => {throwInvite(item)}} src={invited.includes(item.id) ? minus : plus} alt="add" className="invite-app__add" />
                         </div>
                      )
                   })}
                </div>
-               <button className="invite-app__btn">Отправить</button>
+               <button ref={sendBtn} onClick={openPopup} style={invited.length !== 0 ? {display: 'inline'} : {display: 'none'}} className="invite-app__btn">Отправить</button>
             </div>
          </div>
-         <Skeleton />
       </div>
    )
 }
--- src/Main/Utilities/Skeleton.jsx
+++ src/Main/Utilities/Skeleton.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 function Skeleton(props) {
    return(
-   <ContentLoader 
-    speed={2}
-    width={400}
-    height={160}
-    viewBox="0 0 400 160"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-    {...props}
-  >
-    <rect x="48" y="8" rx="3" ry="3" width="88" height="6" /> 
-    <rect x="48" y="26" rx="3" ry="3" width="52" height="6" /> 
-    <circle cx="20" cy="20" r="20" />
-  </ContentLoader>
+      <ContentLoader 
+      speed={2}
+      width={400}
+      height={50}
+      viewBox="0 0 400 50"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+      <rect x="48" y="8" rx="3" ry="3" width="120" height="6" /> 
+      <rect x="48" y="26" rx="3" ry="3" width="70" height="6" /> 
+      <circle cx="20" cy="20" r="20" />
+    </ContentLoader>
    )
 }
 export default Skeleton;
