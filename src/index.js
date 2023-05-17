c220fb119c8ef873cbad15562160a5dc0e94cb67
Дмитрий <Dmitriifrs@gmail.com>
2023-05-17 16:59:08 +0500

    Converter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import services from './services';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App 
+      test={services.quiz} 
+      currency={services.currency}
+      />
+    </Router>
   </React.StrictMode>
 );
 
