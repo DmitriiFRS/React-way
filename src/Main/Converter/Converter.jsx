6c7d5255d8397378a520a7191fa44b35a8aa0ffa
Дмитрий <Dmitriifrs@gmail.com>
2023-06-25 23:47:16 +0500

    added loader

--- src/Main/Converter/Converter.jsx
+++ src/Main/Converter/Converter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../Main/App.scss';
 import replace from './images/replace.svg';
 import InputItem from './InputItem';
+import Loader from '../Utilities/Loader';
 function Converter({currencyNames}) {
    const settings = ['disabled', ''];
    const title = ['Amount', 'Converted Amount'];
@@ -14,10 +15,14 @@ function Converter({currencyNames}) {
    const [outputValue, setOutputValue] = React.useState('');
    const [currency1, setCurrency1] = React.useState([currencyNames[0].name, currencyNames[0].icon])
    const [currency2, setCurrency2] = React.useState([currencyNames[3].name, currencyNames[3].icon])
+   const [loading, setLoading] = React.useState(true)
    React.useEffect(() => {
       fetch('https://api.freecurrencyapi.com/v1/latest?apikey=9iK6i08cBV5rNIDPOACRwikSuRDXTSSkrbqdmAZ2')
       .then(resp => resp.json())
-      .then(data => setDataCurrency(data.data))
+      .then(data => {
+         setDataCurrency(data.data)
+         setLoading(false)
+      })
    }, [])
    React.useEffect(() => {
       setOutputValue((inputValue * rate2 / rate1).toFixed(2))
@@ -30,41 +35,48 @@ function Converter({currencyNames}) {
       <section className="converter container">
          <h2 className="converter__title">Currency Converter</h2>
          <p className="converter__subtitle title">Check live rates, set rate alerts, receive notifications and more.</p>
-         <div className="converter__interface">
-            <InputItem setRate={setRate1}
-               currencyValue={inputValue}
-               currency={currency1}
-               setCurrency={setCurrency1}
-               setCurrencyValue={setInputValue}
-               dataCurrency={dataCurrency}
-               disabled={settings[1]} 
-               active={Active1} 
-               setActive={setActive1} 
-               currencyNames={currencyNames} 
-               title={title[0]}>
-            </InputItem>
-            <div className="interface-item__separator-container">
-               <div className="interface-item__separator"></div>
-               <div onClick={swap} className="interface-item__replace">
-                  <img src={replace} alt="swap" />
+         {loading ? (
+            <Loader />
+         ) : (
+            <>
+               <div className="converter__interface">
+               <InputItem setRate={setRate1}
+                  currencyValue={inputValue}
+                  currency={currency1}
+                  setCurrency={setCurrency1}
+                  setCurrencyValue={setInputValue}
+                  dataCurrency={dataCurrency}
+                  disabled={settings[1]} 
+                  active={Active1} 
+                  setActive={setActive1} 
+                  currencyNames={currencyNames} 
+                  title={title[0]}>
+               </InputItem>
+               <div className="interface-item__separator-container">
+                  <div className="interface-item__separator"></div>
+                  <div onClick={swap} className="interface-item__replace">
+                     <img src={replace} alt="swap" />
+                  </div>
                </div>
+               <InputItem setRate={setRate2}
+                  currencyValue={outputValue}
+                  currency={currency2}
+                  setCurrency={setCurrency2}
+                  dataCurrency={dataCurrency}
+                  disabled={settings[0]}
+                  active={Active2}
+                  setActive={setActive2}
+                  currencyNames={currencyNames}
+                  title={title[1]}>
+               </InputItem>
             </div>
-            <InputItem setRate={setRate2}
-               currencyValue={outputValue}
-               currency={currency2}
-               setCurrency={setCurrency2}
-               dataCurrency={dataCurrency}
-               disabled={settings[0]}
-               active={Active2}
-               setActive={setActive2}
-               currencyNames={currencyNames}
-               title={title[1]}>
-            </InputItem>
-         </div>
-         <div className="converter__exchange-rate-body">
-            <h3 className="converter__exchange-rate-title title">Indicative Exchange Rate</h3>
-            <p className="converter__exchange-rate-result">1 {currency1[0]} = {(rate2 / rate1).toFixed(3)} {currency2[0]}</p>
-         </div>
+            <div className="converter__exchange-rate-body">
+               <h3 className="converter__exchange-rate-title title">Indicative Exchange Rate</h3>
+               <p className="converter__exchange-rate-result">1 {currency1[0]} = {(rate2 / rate1).toFixed(3)} {currency2[0]}</p>
+            </div>
+         </>
+         )}
+         
       </section>
    )
 }
---
+++ src/Main/Utilities/Loader.jsx
@@ -0,0 +1,9 @@
+import '../App.scss';
+function Loader() {
+   return (
+      <div class="loader">
+         
+      </div>
+   )
+}
+export default Loader;
